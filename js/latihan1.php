37f14cfaf61603f8ddbb7ed6b281986c18c45b63
brotosilog <subroto.silog@gmail.com>
2019-03-12 10:59:26 +0700

    gabungan latihan

---
+++ js/latihan1.php
@@ -0,0 +1,6 @@
+<?php
+const SITE_NAME = "https://www.tni.mild.id/";
+
+echo"URL Situs :  " . SITE_NAME;
+
+?>
---
+++ js/latihan2.php
@@ -0,0 +1,15 @@
+<?php
+// membuat konstanta dengan fungsi define()
+define ('DB_SERVER', 'localhost');
+define ('DB_USER', 'root');
+define ('DB_PASS', 'rahasia');
+define ('DB_NAME', 'belajar');
+// membuat konstanta dengan kata kunci const
+const API_KEY ="1234";
+echo"Server: " . DB_SERVER . "<br/>";
+echo"User: " . DB_USER . "<br/>";
+echo"Pass: " . DB_PASS . "<br/>";
+echo"Name: " . DB_NAME . "<br/>";
+echo"API_KEY: " . API_KEY . "<br/>";
+
+?>
